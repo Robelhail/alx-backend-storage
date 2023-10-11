784856768ed54c254b0a20cde20533368677583b
Robelhail <richnrob.2020@gmail.com>
2023-10-11 20:22:10 +0000

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255)
+);
