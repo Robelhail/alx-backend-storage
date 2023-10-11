87c6b8a3688c556795e1299f226c834bc9be8e26
Robelhail <richnrob.2020@gmail.com>
2023-10-11 21:44:41 +0000

    task1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,12 @@
 -- Creates a table users
 -- country, enum of countries: US, CO and TN, never null (default=US)
 -- If table exists, script will not fail
-
-CREATE TABLE IF NOT EXISTS users (
-       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-       email VARCHAR(255) NOT NULL UNIQUE,
-       name VARCHAR(255),
-       country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+-- Drop table `users` if it exists
+DROP TABLE IF EXISTS `users`;
+-- Create table `users` if it does not exists
+CREATE TABLE IF NOT EXISTS `users`(
+       `id` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+       `email` VARCHAR(255) UNIQUE NOT NULL,
+       `name` VARCHAR(255),
+       `country` ENUM('US','CO','TN') NOT NULL DEFAULT 'US'
 );
