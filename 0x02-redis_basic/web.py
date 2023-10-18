50017104c10abb427ac204d839d9305742b73498
Robelhail <richnrob.2020@gmail.com>
2023-10-18 19:59:45 +0000

    update

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,35 +2,51 @@
 """
 Caching request module
 """
-import redis
+
 import requests
+import redis
+import time
 from functools import wraps
 from typing import Callable
 
+# Dictionary to store cached responses
+cache = {}
 
-def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
-        """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
-        if cached_page:
-            return cached_page.decode('utf-8')
-        response = fn(url)
-        client.set(f'{url}', response, 10)
-        return response
-    return wrapper
+# Decorator to implement caching with a given expiration time
+def cache_response(expiration_time=10):
+    def decorator(func):
+        def wrapper(url):
+            # Check if the URL is in the cache and not expired
+            if url in cache and time.time() - cache[url]["timestamp"] < expiration_time:
+                print(f"Using cached content for {url}")
+                cache[url]["count"] += 1
+                return cache[url]["content"]
+            else:
+                content = func(url)
+                # Update cache with new content and timestamp
+                cache[url] = {
+                    "content": content,
+                    "timestamp": time.time(),
+                    "count": 1
+                }
+                return content
+        return wrapper
+    return decorator
 
-
-@track_get_page
-def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
-    """
+# Function to get page content from a given URL
+@cache_response()
+def get_page(url):
     response = requests.get(url)
     return response.text
+
+# Example usage
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk/delay/1000/url/http://www.google.com"
+    content = get_page(url)
+    print(content)
+    print(f"Number of times accessed: {cache[url]['count']}")
+    time.sleep(5)  # Simulating a delay to test caching expiration
+    content = get_page(url)
+    print(content)
+    print(f"Number of times accessed: {cache[url]['count']}")
+
